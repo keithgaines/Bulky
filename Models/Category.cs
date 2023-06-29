c3a90977e57ae1661e3c80a116fd3f844cd64d94
Keith Gaines <patrick.keith.gaines@gmail.com>
2023-06-29 09:55:19 -0500

    added connection string

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,7 +1,12 @@
-﻿namespace ecommerceMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ecommerceMVC.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
     }
 }
