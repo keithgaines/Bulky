a308fe912e2a0d208d5d1e12cb786a826dfcfcd7
Keith Gaines <patrick.keith.gaines@gmail.com>
2023-07-03 14:01:20 -0500

    added server side validation

--- Models/Category.cs
+++ Models/Category.cs
@@ -6,7 +6,9 @@ namespace ecommerceMVC.Models
     {
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100.")]
         public int DisplayOrder { get; set; }
     }
 }
