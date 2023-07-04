89bb09aace5acc32f09be959aa455655e7976753
Keith Gaines <patrick.keith.gaines@gmail.com>
2023-07-04 14:19:13 -0500

    added "get category" to edit page

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -39,5 +39,38 @@ namespace ecommerceMVC.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryfromDb = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (categoryfromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryfromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot match the name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
     }
 }
