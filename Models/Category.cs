51dd224b25f17a876ced56d51393aac5f83b8107
Keith Gaines <patrick.keith.gaines@gmail.com>
2023-07-04 13:51:59 -0500

    added edit and delete buttons

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ecommerceMVC.Models
 {
@@ -9,6 +10,7 @@ namespace ecommerceMVC.Models
         [MaxLength(30)]
         public string Name { get; set; }
         [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100.")]
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
