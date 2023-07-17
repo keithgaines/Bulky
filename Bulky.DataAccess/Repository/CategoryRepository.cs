64cdd5fa4d006f5524a0693ec62aace0f28d583c
Keith Gaines <patrick.keith.gaines@gmail.com>
2023-07-17 11:09:27 -0500

    setup Areas

---
+++ Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using Bulky.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,43 @@
+﻿using Bulky.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            dbSet = _context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return dbSet.ToList();
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            return dbSet.FirstOrDefault(filter);
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+    }
+}
