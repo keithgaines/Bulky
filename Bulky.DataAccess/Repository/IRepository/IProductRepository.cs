1bdadff0eb0b94c90c0870ce5c39a29dd1e61d6b
Keith Gaines <patrick.keith.gaines@gmail.com>
2023-07-17 14:10:43 -0500

    added product table to db and product repository

---
+++ Bulky.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using Bulky.Models.Models;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product obj);
+        void Save();
+    }
+}
---
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Bulky.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models.Models;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _context;
+        public ProductRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
