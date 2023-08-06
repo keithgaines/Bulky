a9409560434c8179c5111ad310acba77decb7c15
Keith Gaines <patrick.keith.gaines@gmail.com>
2023-08-06 14:11:36 -0500

    added identity

--- Bulky/Program.cs
+++ Bulky/Program.cs
@@ -1,8 +1,10 @@
 using Bulky.Data;
 using Bulky.DataAccess.Repository;
 using Bulky.DataAccess.Repository.IRepository;
-
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services; // namespace for email sender service
 using Microsoft.EntityFrameworkCore;
+using YourRazorPagesApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddRazorPages();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+// Replace "your_sendgrid_api_key" with your actual SendGrid API key.
+builder.Services.AddTransient<IEmailSender>(serviceProvider => new EmailSender("your_sendgrid_api_key"));
+
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -31,9 +44,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication(); // Use Authentication BEFORE Authorization
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
